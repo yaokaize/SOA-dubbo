855a89a53677e10b5e02169722faa2c54cb0e144
kaiser <zz_ykz@163.com>
2021-01-15 19:06:29 +0800

    说明文件

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SOA-dubbo
 springboot2.4 + dubbo + zookeeper + mybatis + dubbo-admin
+
+scripts: 项目搭建、版本升级脚本文件
---
+++ scripts/init/docker.sh
@@ -0,0 +1,12 @@
+echo 'docker start zookeeper. doc is https://registry.hub.docker.com/_/zookeeper'
+docker stop cp-zookeeper
+docker run --name cp-zookeeper --restart always -d zookeeper
+sleep 5
+
+echo 'docker start zookeeper. doc is https://registry.hub.docker.com/_/memcached'
+docker stop wawa-memcached
+# 没有设置内存使用率
+docker run --name wawa-memcached -d memcached
+# 设置内存使用率
+# docker run --name wawa-memcached -d memcached memcached -m 64
+sleep 5
