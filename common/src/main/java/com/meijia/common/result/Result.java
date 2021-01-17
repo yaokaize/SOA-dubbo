b0aefb17d9c3bac6d8c775d9a88cf3aea3dfc4bf
h.y <masterFifth@163.com>
2021-01-17 23:14:58 +0800

    [master] add log4j2

---
+++ common/src/main/java/com/meijia/common/result/Result.java
@@ -0,0 +1,45 @@
+package com.meijia.common.result;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Setter
+@Getter
+public class Result implements Serializable {
+
+    private ResultCode resultCode;
+
+    private Object data;
+
+    public static Result success() {
+        Result result = new Result();
+        result.setResultCode(ResultCode.SUCCESS);
+        return result;
+    }
+
+    public static Result success(Object data) {
+        Result result = new Result();
+        result.setResultCode(ResultCode.SUCCESS);
+        result.setData(data);
+        return result;
+    }
+
+    public static Result failure(ResultCode resultCode){
+        Result result = new Result();
+        result.setResultCode(resultCode);
+        return result;
+    }
+
+    public static Result failure(ResultCode resultCode, Object data){
+        Result result = new Result();
+        result.setResultCode(resultCode);
+        result.setData(data);
+        return result;
+    }
+}
