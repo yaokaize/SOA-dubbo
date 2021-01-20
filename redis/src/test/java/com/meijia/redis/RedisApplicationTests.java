af8e020c66210a4fa8ef6a147aeb80414f422cfd
kaiser <zz_ykz@163.com>
2021-01-20 15:41:10 +0800

    [master] 整合redis 以及 mongodb

---
+++ redis/src/test/java/com/meijia/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meijia.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- scripts/init/docker.sh
+++ scripts/init/docker.sh
@@ -3,10 +3,17 @@ docker stop cp-zookeeper
 docker run --name cp-zookeeper -p 2181:2181 --restart always -d zookeeper
 sleep 5
 
-echo 'docker start zookeeper. doc is https://registry.hub.docker.com/_/memcached'
+echo 'docker start memcached. doc is https://registry.hub.docker.com/_/memcached'
 docker stop wawa-memcached
 # 没有设置内存使用率
-docker run --name wawa-memcached -d memcached
+docker run --name wawa-memcached -p 11211:11211 -d memcached
 # 设置内存使用率
 # docker run --name wawa-memcached -d memcached memcached -m 64
+sleep 5
+
+echo 'docker start redis doc is https://registry.hub.docker.com/_/redis'
+docker stop wawa-redis
+docker run -itd --name wawa-redis -p 6379:6379 redis
+# 查看redis数据命令
+# docker exec -it wawa-redis /bin/bash
 sleep 5
